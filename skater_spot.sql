60d2221283f0506414437b0357fca770e576fee1
bhernandezortiz <neybrian11@gmail.com>
2023-01-31 12:18:57 -0800

    updated

--- skater_spot.sql
+++ skater_spot.sql
@@ -1,5 +1,5 @@
-DROP TABLE IF EXISTS skater_spots2;
-CREATE TABLE skater_spots2(
+DROP TABLE IF EXISTS skater_spots;
+CREATE TABLE skater_spots(
     id serial,
     location_name varchar (50) NOT NULL,
     distance integer NOT NULL,
@@ -7,4 +7,4 @@ CREATE TABLE skater_spots2(
     PRIMARY KEY (id)
 );
 
-INSERT INTO skater_spots2 (location_name, distance, description) VALUES ('Bresemann Forest Park Trail',11, 'This naturally wooded site is approximately 70 acres with a system of pedestrian nature trails within the Parkland/Spanaway area. The beautiful and secluded walking trail may have users thinking they are miles from the city as they stroll along Morey Creek. Spanaway Creek is  a small stream the also runs through Breesemann Forest from Spanaway Lake and is part of the Clover Creek watershed. Pavement is smooth enough to ride comfortably. Paths can become hilly, watch your speed and try not to hit any pedestrians.');
+INSERT INTO skater_spots (location_name, distance, description) VALUES ('Bresemann Forest Park Trail',11, 'This naturally wooded site is approximately 70 acres with a system of pedestrian nature trails within the Parkland/Spanaway area. The beautiful and secluded walking trail may have users thinking they are miles from the city as they stroll along Morey Creek. Spanaway Creek is  a small stream the also runs through Breesemann Forest from Spanaway Lake and is part of the Clover Creek watershed. Pavement is smooth enough to ride comfortably. Paths can become hilly, watch your speed and try not to hit any pedestrians.');
