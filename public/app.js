3557f64049cfb4920c2a2e776ef1b48b1b301237
bhernandezortiz <neybrian11@gmail.com>
2023-02-01 13:53:54 -0800

    post fixed

--- public/app.js
+++ public/app.js
@@ -1,7 +1,12 @@
-const container = $('#container')
+const $post = $('#post-btn')
+const $location_name = $('#location_name')
+const $distance = $('#distance')
+const $description = $('#description')
 
+const container = $('#container')
+//http://localhost:8000
 let loadSpots = async () => {
-    await fetch('/home')
+    await fetch('http://localhost:8000/home')
     .then ((response)=> response.json())
     .then ((data) => {
         data.forEach((element) => {
@@ -11,14 +16,44 @@ let loadSpots = async () => {
                 <div class = name><strong>${element.location_name}</strong></div>
                 <div class = distance>${element.distance} mi </div>
                 <div class = description>${element.description}</div>
-                <form action="${element.google_maps}">
-                <button class = dirbutton type="submit">Directions</button>
-             </form>
             </div>`)
 
             bodydata.appendTo($('#container'))
         })
     })
 }
+
+
+$post.on('click', async(e) => {
+    console.log($description.val())
+    try{
+        const response = await fetch('http://localhost:8000/home',{
+            method: 'POST',
+            headers: {
+                'Content-Type':'application/json'
+            },
+            body: JSON.stringify({
+                "location_name":`${$location_name.val()}`,
+                "distance":`${$distance.val()}`,
+                "description":`${$description.val()}`
+            })
+        }).then((response) => {
+            console.log(response)
+            return response.json()
+        }).then((data)=>{
+            console.log(data)
+            let newInfo = $(`
+        <div class="card">
+            <div class = name><strong>${data.location_name}</strong></div>
+            <div class = distance>${data.distance} mi </div>
+            <div class = description>${data.description}</div>
+        </div>`)
+
+        newInfo.appendTo($('#container'))
+        })
+    }catch(error){
+        console.log(error.message)
+    }
+})
 
 loadSpots()
--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const app = express();
 const port = process.env.PORT || 8000;
 const client = require('./db')
 const dotenv = require('dotenv');
-// const cors = require('cors')
+const cors = require('cors')
 dotenv.config();
 
 app.use(express.json())
-// app.use(cors())
+app.use(cors())
 
 app.route('/home')
     .get(async (req, res) => {
@@ -18,10 +18,20 @@ app.route('/home')
             console.log(error.message)
         }
     })
+    .post(async (req, res) =>{
+        console.log(req.body)
+        try {
+            console.log(req.body)
+            const body = req.body
+            await client.query(`INSERT INTO skater_spots (location_name, distance, description) VALUES ('${body.location_name}', '${body.distance}', '${body.description}')`);
+            res.status(200).type('application/json').json(body); 
+        } catch (error) {
+            res.status(500).type('text/plain').send(error.message)
+        }
+    })
 
 app.use(express.static('./public'));
 
-//listener on port
 app.listen(port,() => {
     console.log(`Listening on Port: ${port}`)
 })
